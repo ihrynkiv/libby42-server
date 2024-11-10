c0f185aa578ef3780626b8dd51dd5b9a53e652c3
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2024-11-10 15:08:34 +0200

    create users crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from "@prisma/prisma.service";
-import { UsersModule } from "@modules/users.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
